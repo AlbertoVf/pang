84b62c087fb15f8903f30a28112f82384477e86f
AlbertoVf <AlbertoVf97@protonmail.com>
2019-08-01 16:33:48 +0200

    Personaje
    Creacion de personaje y movimiento basico

---
+++ src/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speed = 4f;
+    float movement = 0;
+    private Rigidbody2D rb;
+    private Animator animator;
+    SpriteRenderer sr;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal") * speed;
+        animator.SetInteger("velX", Mathf.RoundToInt(movement));
+        if (movement < 0)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + Vector2.right * movement * Time.fixedDeltaTime);
+    }
+}
